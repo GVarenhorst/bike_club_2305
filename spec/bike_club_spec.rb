afc8da23b8a8f4dcdc586508742042ca1b891ca5
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-06-05 11:42:13 -0500

    test: Add and pass initialize test

---
+++ spec/bike_club_spec.rb
@@ -0,0 +1,20 @@
+require './lib/ride'
+require './lib/biker'
+require './lib/bike_club'
+
+RSpec.describe BikeClub do 
+  before(:each) do
+    @bike_club =BikeClub.new("Rolling Cycle")
+    @biker1 = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+  end
+
+  describe "#Iteration 3" do
+    it "can initialize and has attributes" do
+
+      expect(@bike_club).to be_an_instance_of(BikeClub)
+    end
+  end
+end
