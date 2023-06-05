59eeb23f6cbb62e951e8194893a5e7b5615cdd7e
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-06-05 13:03:37 -0500

    Add most_rides method

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,9 +1,22 @@
 class BikeClub
   attr_reader :name,
               :roster
-              
+
   def initialize(name)
   @name = name
   @roster = []
+  end
+
+  def add_roster(biker)
+    @roster << biker
+  end
+
+  def most_rides(bikers)
+    dedicated_rider = []
+    roster.each do |biker|
+      biker.log_ride.count
+      dedicated_rider << biker.first
+
+    end
   end
 end
