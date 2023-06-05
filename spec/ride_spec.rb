5b40a0dc8709803fa447de3e27f28e3cc4a8698b
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-06-05 10:32:15 -0500

    test: Add total distance test

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Ride do
     @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
     @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
-  describe '#initialize' do
+  describe '#Iteration 1' do
     it 'exists' do
 
       expect(@ride1).to be_a(Ride)
@@ -25,6 +25,9 @@ RSpec.describe Ride do
       expect(@ride2.loop?).to eq(true)
     end
 
-
+    it "can determine total distance" do
+      expect(@ride1.total_distance).to eq(21.4)
+      expect(@ride2.total_distance).to eq(14.9)
+    end
   end
 end
